345b13479b5694f83315f54ff612eba07a3ecffb
James Kurczodyna <james@wearefine.com>
2015-02-07 15:33:24 -0800

    prep gem for fresh install - update dependency versions - move secrets setup to - add install docs

--- lib/calagator/engine.rb
+++ lib/calagator/engine.rb
@@ -1,5 +1,10 @@
 module Calagator
   class Engine < ::Rails::Engine
     isolate_namespace Calagator
+
+    config.before_initialize do
+      require 'secrets_reader'
+      ::SECRETS = SecretsReader.read
+    end
   end
 end
